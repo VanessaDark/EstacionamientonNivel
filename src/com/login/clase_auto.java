ad016e76177902e79d90d995601689819fe12349
DarkLigihtt <vancraft04@gmail.com>
2023-05-07 23:47:19 -0600

    class_auto modificacion

---
+++ src/com/login/clase_auto.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.login;
+
+/**
+ *
+ * @author vanes
+ */
+public class clase_auto {
+    
+     //Atributos auto
+    private String placa;
+    private String tipo;
+    private String marca;
+    private int modelo;
+    private String color;
+    private String hora;
+    private String fecha;
+
+    //Atributos de usuario
+    private String propietario;
+    private int telefono;
+    private String direccion;
+    
+    //Atributos del lugar
+    private String disponibolidad;
+    private int lugar;
+    
+}
